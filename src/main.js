b6820ad877dc14e6f37d9c9fcb3024b637e3bf87
75pearsinabunch <75pearsinabunch@gmail.com>
2021-04-23 18:01:55 -0700

    js

--- src/main.js
+++ src/main.js
@@ -21,3 +21,29 @@ let gameOver = false;
 let failText1, failText2, failText3;
 let p1Rocket;
 let p1Score;
+
+
+
+
+
+
+console.log("Alvaro Perez");
+console.log("Rocket Patrol Mod: Slime Invasion");
+console.log("4/19/2021");
+console.log("Estmated working time: 15 hours");
+
+console.log("Score Breakdown:");
+console.log("Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi) (60)");
+console.log("Create a new spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (20)");
+console.log("Create new artwork for all of the in-game assets (rocket, spaceships, explosion) (20) ");
+console.log("Use Phaser's particle emitter to create a particle explosion when the rocket hits the spaceship (20)");
+console.log("Display the time remaining (in seconds) on the screen (10)");
+console.log("Create a new title screen (e.g., new artwork, typography, layout) (10)");
+
+console.log("Citation:");
+console.log("https://freesound.org/people/broumbroum/sounds/50561/");
+console.log("https://freesound.org/people/j1987/sounds/335745/");
+console.log("https://freesound.org/people/zgump/sounds/86330/");
+console.log("https://freesound.org/people/renatalmar/sounds/264981/");
+console.log("https://jsfiddle.net/munibmir/mqcs47w9/1/");
+console.log("https://phaser.io/examples/v3/view/physics/arcade/bullets-group");
